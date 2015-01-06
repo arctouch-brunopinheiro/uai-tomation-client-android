1cd88f5a4ef87ae688072551103487541b6eec64
arctouch-brunopinheiro <bruno.pinheiro@arctouch.com>
2015-01-06 15:49:42 -0200

    Created android version of the control app for the arduino automation tools

---
+++ app/src/main/java/uaitomation/arctouch/com/uaitomation/ArduinoService.java
@@ -0,0 +1,73 @@
+package uaitomation.arctouch.com.uaitomation;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by brunopinheiro on 1/6/15.
+ */
+public class ArduinoService {
+
+    public static ArduinoService INSTANCE;
+    private final RequestQueue requestQueue;
+    private Context context;
+    private String serverAddress;
+
+    public static ArduinoService instance(Context context, String serverAddress) {
+        if (INSTANCE == null) {
+            INSTANCE = new ArduinoService();
+        }
+        INSTANCE.context = context;
+        INSTANCE.serverAddress = serverAddress;
+        return INSTANCE;
+    }
+
+    private ArduinoService() {
+        requestQueue = Volley.newRequestQueue(context);
+    }
+
+    public void setAirConditionerState (boolean on, Response.Listener<String> callback) {
+        // Instantiate the RequestQueue.
+        String url = baseServerUrl();
+        url+= on ? "?cold" : "?off";
+
+        doRequest(url, callback);
+    }
+
+    public void setAirConditionerWarm(Response.Listener<String> callback) {
+        String url = baseServerUrl();
+        url+= "?warm";
+
+        doRequest(url, callback);
+    }
+
+    public void setAirConditionerCold(Response.Listener<String> callback) {
+        String url = baseServerUrl();
+        url+= "?cold";
+
+        doRequest(url, callback);
+    }
+
+    private void doRequest(String url, Response.Listener<String> callback) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                callback, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(this.getClass().getName(), "Command failed with error " + error.getLocalizedMessage());
+            }
+        });
+
+        requestQueue.add(stringRequest);
+    }
+
+    private String baseServerUrl() {
+        return "http://" + serverAddress + "/";
+    }
+}
---
+++ app/src/main/java/uaitomation/arctouch/com/uaitomation/HomeControlActivity.java
@@ -0,0 +1,103 @@
+package uaitomation.arctouch.com.uaitomation;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import com.android.volley.Response;
+
+
+public class HomeControlActivity extends ActionBarActivity {
+
+    EditText serverAddress;
+    Switch airConditionerStateSwitch;
+    Button airConditionerWarmButton;
+    Button airConditionerColdButton;
+    private ArduinoService arduinoService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_control);
+
+        serverAddress = (EditText) findViewById(R.id.serverAddress);
+        airConditionerStateSwitch = (Switch) findViewById(R.id.airConditionerStateSwitch);
+        airConditionerWarmButton = (Button) findViewById(R.id.airConditionerWarmButton);
+        airConditionerColdButton = (Button) findViewById(R.id.airConditionerColdButton);
+
+        arduinoService = ArduinoService.instance(this, this.serverAddress.getText().toString());
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_control, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void airConditionerWarmButtonClick(View view) {
+        showDialog("Air conditioner warm button clicked");
+        arduinoService.setAirConditionerWarm(new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                // HANDLE CALLBACK
+            }
+        });
+    }
+
+    public void airConditionerColdButtonClick(View view) {
+        showDialog("Air conditioner cold button clicked");
+        arduinoService.setAirConditionerCold(new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                // HANDLE CALLBACK
+            }
+        });
+    }
+
+    public void airConditionerStateSwitchClick(View view) {
+        showDialog("Air conditioner state switch clicked");
+        arduinoService.setAirConditionerState(airConditionerStateSwitch.isChecked(), new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                // HANDLE CALLBACK
+            }
+        });
+    }
+
+    private void showDialog(String message) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+}
